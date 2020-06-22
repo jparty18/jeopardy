2cfa3721c07a08a54398dfd32737b39fcdc4067c
weann2020 <anw2@unlv.nevada.edu>
2020-06-22 09:04:42 -0700

    Players files

---
+++ src/com/jeopardy/Players.java
@@ -0,0 +1,41 @@
+package Players;
+
+  public abstract class Players implements Assistance{
+    //instance variables
+    private String name;
+    private Expertise level;
+
+    //ctors
+    public Players (){
+
+    }
+
+    public Players (String name, Expertise level) {
+      setName(name);
+      setLevel(level);
+    }
+
+    //business methods
+    public abstract int calculateFinalScore();
+
+    //accessor methods
+    public String getName() {
+      return name;
+    }
+    public void setName(String name) {
+      this.name = name;
+    }
+
+    public Expertise getLevel() {
+      return level;
+    }
+    public void setLevel(Expertise level) {
+      this.level = level;
+    }
+
+    //toString()
+    public String toString() {
+      return getClass().getSimpleName() + ": name=" + getName() + " level=" + getLevel();
+    }
+
+}
